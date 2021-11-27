c9b278b54d1a43abbbd39090e904e2307a45e5a5
Thomas George <private@thomas-george.com>
2021-11-27 17:04:12 +0100

    Initial Commit

---
+++ touchdown/touchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  touchdownApp.swift
+//  touchdown
+//
+//  Created by Thomas George on 27/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct touchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

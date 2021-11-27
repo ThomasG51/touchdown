e99d5503c18d3c64a7c29fb8c39f044f3877ee98
Thomas George <private@thomas-george.com>
2021-11-27 17:32:49 +0100

    configuration du projet

---
+++ touchdown/Utility/Constant.swift
@@ -0,0 +1,28 @@
+//
+//  Constant.swift
+//  touchdown
+//
+//  Created by Thomas George on 27/11/2021.
+//
+
+import SwiftUI
+
+// DATA
+
+// COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+
+// UX
+
+// API
+
+// IMAGE
+
+// FONT
+
+// STRING
+
+// MISC
